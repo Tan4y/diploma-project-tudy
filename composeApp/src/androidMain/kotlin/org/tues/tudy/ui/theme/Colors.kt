9498d062fa7cff570fae157bbb239be0072e86c2
Tan4y <100035220+Tan4y@users.noreply.github.com>
2025-11-28 00:56:23 +0200

    Sync files with gradle

--- composeApp/src/androidMain/kotlin/org/tues/tudy/ui/theme/Colors.kt
+++ composeApp/src/androidMain/kotlin/org/tues/tudy/ui/theme/Colors.kt
@@ -4,14 +4,14 @@ import androidx.compose.ui.graphics.Color
 val PrimaryColor1 = Color(0xFFFF842C)
 val PrimaryColor2 = Color(0xFFFFC02E)
 
-val SecondaryColor = Color(0xFFF2F2F2)
+val SecondaryColor1 = Color(0xFFF2F2F2)
 
-val Base0Color = Color(0xFFFFFCFA)
-val Base20Color = Color(0xFFE6E6E6)
-val Base40Color = Color(0xFFD1D1D1)
-val Base60Color = Color(0xFFABABAB)
-val Base80Color = Color(0xFF7D7D7D)
-val Base100Color = Color(0xFF221F1F)
+val BaseColor0 = Color(0xFFFFFCFA)
+val BaseColor20 = Color(0xFFE6E6E6)
+val BaseColor40 = Color(0xFFD1D1D1)
+val BaseColor60 = Color(0xFFABABAB)
+val BaseColor80 = Color(0xFF7D7D7D)
+val BaseColor100 = Color(0xFF221F1F)
 
 val SuccessColor = Color(0xFF10E260)
 val WarningColor = Color(0xFFFFA323)
