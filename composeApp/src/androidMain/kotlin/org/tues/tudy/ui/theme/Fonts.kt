bf2707d1ff6fcab2a1acc6f68e3b32a2dbcb92de
Tan4y <100035220+Tan4y@users.noreply.github.com>
2025-11-27 18:13:51 +0200

    Fonts.kt
    - create all headings, bodies, paragraphs and captions

---
+++ composeApp/src/androidMain/kotlin/org/tues/tudy/ui/theme/Fonts.kt
@@ -0,0 +1,15 @@
+package org.tues.tudy.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import org.tues.tudy.R
+
+// Fredoka – only one file, used for headings/titles
+val PrimaryFont = FontFamily(
+    Font(R.font.fredoka)  // single file
+)
+
+// Lato – only one file, used for body text
+val SecondaryFont = FontFamily(
+    Font(R.font.lato)     // single file
+)
