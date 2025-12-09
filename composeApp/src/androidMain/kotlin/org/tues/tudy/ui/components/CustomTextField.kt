d5ac77f78fef16fb376f4f201bc65668e60f2659
Tan4y <100035220+Tan4y@users.noreply.github.com>
2025-12-09 16:24:23 +0200

    Remove unnecessarily commends in CustomTextField.kt

--- composeApp/src/androidMain/kotlin/org/tues/tudy/ui/components/CustomTextField.kt
+++ composeApp/src/androidMain/kotlin/org/tues/tudy/ui/components/CustomTextField.kt
@@ -7,16 +7,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -33,7 +30,6 @@ fun CustomTextField(
     error: String? = null,
 ) {
     val isFocused = remember { mutableStateOf(false) }
-    val shouldFloat = isFocused.value || value.isNotEmpty()
 
     val stateColor = when {
         error != null -> ErrorColor
